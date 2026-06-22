17993fbdf52c85e5fc8dedfdfc43f137180d7fe6
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2026-06-22 15:35:50 +0600

    feat: user and profile schema added

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import app from "./app"
 import config from "./config"
+import { prisma } from "./lib/prisma"
 
 const main = async()=>{
     try {
+        await prisma.$connect()
+        console.log("Database connected successfully")
         app.listen(config.port, ()=>{
             console.log(`Server running at at port ${config.port}`)
         })
     } catch (error) {
+        await prisma.$disconnect()
         console.error("Error starting the server:", error);
         process.exit(1);
         
