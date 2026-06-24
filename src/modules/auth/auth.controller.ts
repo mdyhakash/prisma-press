a77acaa5721ab8a96ca44917a244955110554f7c
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2026-06-24 22:39:41 +0600

    feat: set access and refresh token to the cookies

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,12 +7,25 @@ import { authService } from "./auth.service";
 const loginUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-    const loginResult = await authService.loginUser(payload);
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24, // 1 day in ms
+    });
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 day in ms
+    });
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
       message: "User logged in successfully",
-      data: loginResult,
+      data: { accessToken, refreshToken },
     });
   },
 );
