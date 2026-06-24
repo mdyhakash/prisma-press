ef7a6265ef2fbde45b27573dabb11f3d35da84e6
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2026-06-24 20:12:24 +0600

    feat: add generic sendResponse utility function

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,12 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import httpStatus from "http-status";
+import { sendResponse } from "../../utils/sendResponse";
+
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
     const user = await userService.registerUserIntoDB(payload);
 
-    res.status(httpStatus.CREATED).json({
+    sendResponse(res, {
       success: true,
       statusCode: httpStatus.CREATED,
       message: "User registered successfully",
