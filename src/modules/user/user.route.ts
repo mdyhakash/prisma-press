70dec41b0da755f82604157d7db4bd919142e122
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2026-06-25 14:32:55 +0600

    feat: implement auth guard middleware

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,42 @@
-import { Router } from "express";
+import httpStatus from "http-status";
+import { NextFunction, Request, Response, Router } from "express";
 import { userController } from "./user.controller";
+import { jwtUtils } from "../../utils/jwt";
+import config from "../../config";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
-router.get("/me", userController.getMyProfile);
+router.get(
+  "/me",
+  (req: Request, res: Response, next: NextFunction) => {
+    const { accessToken } = req.cookies;
+
+    const verifiedToken = jwtUtils.verifyToken(
+      accessToken,
+      config.jwt_access_secret,
+    );
+
+    const { email, name, id, role } = verifiedToken;
+    const requiredRoles = [Role.ADMIN, Role.User, Role.AUTHOR];
+    if (!requiredRoles.includes(role)) {
+      return res.status(403).json({
+        success: false,
+        statusCode: httpStatus.FORBIDDEN,
+        message: "Forbidden. You don't have permission",
+      });
+    }
+
+    req.user = {
+      name,
+      email,
+      id,
+      role,
+    };
+    next();
+  },
+  userController.getMyProfile,
+);
 
 export const userRouter = router;
